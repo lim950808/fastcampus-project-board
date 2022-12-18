5ffb712c2f1f6455e92eea859d2ea2bd10bceef9
jaeeoklim <lim080895@gmail.com>
2022-12-18 14:46:25 +0900

    #16 - 컨트롤러 테스트 업데이트
    미디어 타입 검사 방법을  c3b6e7e 에서 확인했기 때문에,
    나머지 뷰도 동일하게 작동할 것으로 예상하고
    머리 테스트 메소드 업데이트

--- src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -45,7 +45,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
